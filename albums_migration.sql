4f9402e46af1a76da0f79af74d4dcf8f81f989c4
Moriah Humphries <moriah.humphries@gmail.com>
2020-05-26 12:27:10 -0500

    setting up album table

--- albums_migration.sql
+++ albums_migration.sql
@@ -6,7 +6,7 @@ drop table if exists albums;
 create table albums (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     artist varchar(100) not null,
-    name carchar(100) not null,
+    name varchar(100) not null,
     release_date int not null,
     sales decimal(6, 3) not null,
     genre varchar(100),
