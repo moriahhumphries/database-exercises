edf258e863ca2dc05ff873e9ffe785d295cb00b7
Moriah Humphries <moriah.humphries@gmail.com>
2020-05-28 23:01:17 -0500

    updated captions on exercise

--- delete_exercises.sql
+++ delete_exercises.sql
@@ -1,8 +1,7 @@
--- select * from albums where release_date > 1991;
+use codeup_test_db;
+
 delete from albums where release_date > 1991;
 
--- select * from albums where genre = 'disco';
 delete from albums where genre = 'disco';
 
--- select * from albums where artist = 'James Horner';
 delete from albums where artist = 'James Horner';
