6f0116ac9b7078214df7b22b9a5fd2663d3a436b
Moriah Humphries <moriah.humphries@gmail.com>
2020-05-29 14:56:13 -0500

    completed exercise

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -1,5 +1,6 @@
 use employees;
 
+#1
 SELECT *
 FROM employees
 WHERE hire_date IN (
@@ -7,3 +8,19 @@ WHERE hire_date IN (
     FROM employees
     where emp_no = 101010
 );
+
+SELECT distinct title
+from titles
+where emp_no in (
+    select emp_no
+    from employees
+    where first_name = 'Aamod'
+    );
+
+SELECT first_name, last_name
+FROM employees
+WHERE emp_no IN (
+    SELECT emp_no
+    FROM dept_manager
+    WHERE to_date > NOW()
+) AND gender = 'F';
